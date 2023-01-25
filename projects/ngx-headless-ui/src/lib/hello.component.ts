3cc520fff2d2d695cab09da7e04035987af01275
Timothy Alcaide <timothy.alcaide@gmail.com>
2023-01-25 20:12:51 +0100

    add library project

---
+++ projects/ngx-headless-ui/src/lib/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'hui-hello',
+  template: `<p>hui-hello works!</p>`,
+})
+export class HelloComponent {}
---
+++ projects/ngx-headless-ui/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-headless-ui
+ */
+
+export * from './lib/hello.component';
