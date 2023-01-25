90a292043ca68a1439f50c4cb473a26d681889d1
Timothy Alcaide <timothy.alcaide@gmail.com>
2023-01-25 20:21:33 +0100

    add demo project

---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HelloComponent } from 'projects/ngx-headless-ui/src/public-api';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HelloComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
